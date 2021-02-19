a5c6bc3e6bbc979149d92ee086f4cb3932621d9e
ASDAFF <asdaff.asad@yandex.ru>
2021-02-19 23:12:03 +0500

    add noFeedback

---
+++ source/noFeedback/acrit.core/lang/ru/install/index.php
@@ -0,0 +1,8 @@
+<?
+$MESS['ACRIT_CORE_MODULE_NAME'] = 'Служебные инструменты "Работы с данными"';
+$MESS['ACRIT_CORE_MODULE_DESC'] = 'Модуль служебных инструментов.';
+$MESS['ACRIT_CORE_PARTNER_NAME'] = 'ASDAFF';
+$MESS['ACRIT_CORE_PARTNER_URI'] = 'https://asdaff.github.io/';
+$MESS['ACRIT_CORE_RECOMMENDED'] = 'Рекомендации';
+$MESS['ACRIT_CORE_UNINSTALL_TITLE'] = 'Деинсталляция модуля «'.$MESS['ACRIT_CORE_MODULE_NAME'].'»';
+?>
---
+++ source/noFeedback/acrit.core/lang/ru/install/uninst_form.php
@@ -0,0 +1,7 @@
+<?
+$MESS ['acrit.uninst_fb_DEL'] = 'Удалить модуль';
+$MESS ['acrit.uninst_fb_ERROR'] = 'Ошибка!';
+$MESS ['acrit.uninst_fb_ERROR_TEXT'] = 'Не указана причина удаления модуля';
+$MESS ['acrit.uninst_fb_MOD_UNINST_OK'] = 'Модуль успешно удален';
+$MESS ['acrit.uninst_fb_PAGE_TITLE'] = 'Удаление модуля "#MODULE_NAME#"';
+?>
